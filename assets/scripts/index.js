d84acd3194501af31fa00cbfb8732e27ae056b53
GyuBin516 <alsl2698@naver.com>
2025-03-28 13:24:09 +0900

    first commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.scroll').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
